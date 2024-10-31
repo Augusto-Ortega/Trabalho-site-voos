3ad9e05be7ce794885a209201949d14e459bafab
Augusto <89861813+Augusto-Ortega@users.noreply.github.com>
2024-10-30 22:12:02 -0300

    Up
    up

--- novo/Projeto TI/js/Rota.js
+++
@@ -1,4 +0,0 @@
-// Rota GET para /register
-app.get('/register', (req, res) => {
-    res.send('Por favor, use o método POST para registrar um usuário.');
-});
--- novo/Projeto TI/js/finalisado.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('newPurchase').addEventListener('click', function() {
-    // Redireciona para a página inicial ou de compras
-    window.location.href = '../html/index.html'; // Substitua pelo caminho da sua página inicial
-});
